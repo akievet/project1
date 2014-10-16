9008f1befcfce1cf5038d05bfaad55931bc0ba75
akievet <akievet@gmail.com>
2014-10-15 20:00:12 -0400

    Sorts add order menu options by course

--- app.rb
+++ app.rb
@@ -20,9 +20,6 @@ end
 ## WL: Organize by meal ['breakfast', 'lunch', 'brunch', 'dinner', etc]
 ## WL: Seperated in dropdown lists by course ['appetizers', 'drinks', 'salads', 'etc']
 get '/menu_items' do
-	# @menu_items = MenuItem.all
-
-
 	@sorted = {}
 	courses=['Salumi, Etc', 'Market/Garden', 'Kitchen', 'Pizza', 'Pizza Toppings',
 		'Sandwiches', 'Pasta', 'Bakery/Pastry', 'Brunch', 'Alcoholic Drinks', 
@@ -125,7 +122,14 @@ end
 # Displays a form for a new order
 get '/parties/:id/orders/new' do
 	@party= Party.find(params[:id]) 
-	@menu_items= MenuItem.all 
+	@sorted = {}
+	courses=['Salumi, Etc', 'Market/Garden', 'Kitchen', 'Pizza', 'Pizza Toppings',
+		'Sandwiches', 'Pasta', 'Bakery/Pastry', 'Brunch', 'Alcoholic Drinks', 
+		'Non-Alcoholic Drinks']
+	courses.each do |course|
+		@sorted[course] = MenuItem.where(course: course)
+	end
+	# @menu_items= MenuItem.all 
 	erb :"orders/new"
 end
 
